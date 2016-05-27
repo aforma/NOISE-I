09688d463284138f020c598e25594b8d1d2d0bd4
Giulian Drimba <giuliandrimba@gmail.com>
2016-05-27 18:44:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## DRAFT
+
+Boilerplate for generative artwork
+
+### Includes
+* ES6 support
+* CSS concatenation
+* HTTP Server
+
+Run `npm start` to start the server.
+
---
+++ src/scripts/sketch.js
@@ -0,0 +1,11 @@
+export default function(p){
+
+  p.setup = function() {
+    p.pixelDensity(window.devicePixelRatio);
+    p.createCanvas(window.innerWidth, window.innerHeight);
+  }
+
+  p.draw = function() {
+
+  }
+}

6f6820f227d698a62883ae45b25fb3345642051f
Giulian Drimba <giuliandrimba@gmail.com>
2016-06-05 01:51:56 -0300

    lots of stuff

--- src/scripts/print.js
+++ src/scripts/print.js
@@ -1,15 +1,31 @@
 var fs = require('fs');
+var path = require("path")
+var colors = require('colors');
+var moment = require("moment");
+var config = require("../../config.json")
 var sketch = require("./sketch");
-
+var loop = require('raf-loop');
 var Canvas = require('canvas');
-var canvas = new Canvas(4724, 4724);
+
+var canvas = new Canvas(parseInt(config.width), parseInt(config.height));
 var ctx = canvas.getContext('2d');
 
-sketch(ctx, function(){
-  var out = fs.createWriteStream(__dirname + '/artwork.jpg');
+sketch.setup(ctx, finish);
+var engine = loop(function(dt) { 
+  sketch.draw() 
+}).start()
+
+console.log("Artwork started at ".yellow + moment().format("HH:mm:ss").green)
+
+function finish(){
+  engine.stop()
+  var filename = config.name + "_" + moment().format("YYYYMMDDHHmmss") + ".jpg";
+  var artworkPath = path.resolve(__dirname, '..', '..', 'print') + '/'+filename
+  var out = fs.createWriteStream(artworkPath);
   var stream = canvas.jpegStream();
 
   stream.on('data', function(chunk){
     out.write(chunk);
   });
-}, true);
+  console.log("Artwork saved at ".green + artworkPath.toString().green)
+};
